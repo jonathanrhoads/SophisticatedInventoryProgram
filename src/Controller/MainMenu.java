77753d94f5491e5696de13ff6f42b6c16e885fe6
Jon Rhoads <jonathan.r.rhoads@outlook.com>
2021-09-12 22:06:58 -0500

    Adding controls for main menu

--- src/Controller/MainMenu.java
+++ src/Controller/MainMenu.java
@@ -1,14 +1,87 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenu implements Initializable {
 
+    public TableColumn partIdCol;
+    public TableColumn partNameCol;
+    public TableColumn partInventoryLevelCol;
+    public TableColumn partPriceCol;
+    public TableColumn productIdCol;
+    public TableColumn productNameCol;
+    public TableColumn productInventoryLevelCol;
+    public TableColumn productPriceCol;
+    public Button addPart;
+    public Button modifyPart;
+    public Button deletePart;
+    public Button addProduct;
+    public Button modifyProduct;
+    public Button deleteProduct;
+    public Button exit;
+    public TableView partTable;
+    public TableView productTable;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Hey y'all!");
+    }
+
+    public void onAddPart(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/AddPartForm.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 1600);
+        stage.setTitle("Add Part");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onModifyPart(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/ModifyPartForm.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 800);
+        stage.setTitle("Modify Part");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onDeletePart(ActionEvent actionEvent) {
+    }
+
+    public void onAddProduct(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/AddProductForm.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 800);
+        stage.setTitle("Add Product");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onModifyProduct(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/ModifyProductForm.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 800);
+        stage.setTitle("Modify Product");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onDeleteProduct(ActionEvent actionEvent) {
+    }
+
+    public void onExit(ActionEvent actionEvent) {
     }
 }
